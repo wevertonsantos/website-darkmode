428e6ba47ecb06cd4b659ce041425d327f13f979
Weverton dos Santos <wever-santos@hotmail.com>
2023-02-21 15:55:46 -0300

    js and responsive toggle

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('.header_toggle')
+const icon = document.querySelector('i')
+
+// text 
+const text = document.querySelector('.header_text')
+text.textContent = 'Home'
+
+// button
+button.addEventListener('click', () => {
+
+})
+
+// icon
+icon.classList.add('fa-solid', 'fa-moon')
